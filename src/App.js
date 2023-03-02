29251a84599a53df1c9ce13efd06655b91f3f9c0
Vincent Toon <vincenttoon22@gmail.com>
2023-03-02 16:46:56 -0600

    app state functioning

--- src/App.js
+++ src/App.js
@@ -6,26 +6,37 @@ import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("imperial");
+  const [weather, setWeather] = useState(null);
 
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data)
-  }
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({...query, units}).then((data) => {
+        setWeather(data);
+      });
+    };
 
-  fetchWeather();
+    fetchWeather();
+  }, [query, units]);
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButton />
       <Inputs />
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
+      {weather && (
+        <div>
+          <TimeAndLocation />
+          <TemperatureAndDetails />
 
-      <Forecast title="hourly forecast" />
-      <Forecast title="daily forecast" />
+          <Forecast title="hourly forecast" />
+          <Forecast title="daily forecast" />
+        </div>
+      )}
     </div>
   );
 }
