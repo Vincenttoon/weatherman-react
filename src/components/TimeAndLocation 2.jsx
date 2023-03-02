1104423e843dd117a2754fb7c49fa1e895e6e155
Vincent Toon <vincenttoon22@gmail.com>
2023-03-02 16:58:33 -0600

    local weather working in metric

--- src/components/TimeAndLocation 2.jsx
+++ src/components/TimeAndLocation 2.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { formatToLocalTime } from '../services/weatherService'
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
         <div className='flex items-center justify-center my-6'>
 
             <p className='text-white text-xl font-extralight'>
-                Thursday, 2 March 2023 | Local Time: 2:52 PM
+                {formatToLocalTime(dt, timezone)}
             </p>
 
         </div>
 
         <div className='flex items-center justify-center my-3'>
             <p className='text-white text-3xl font-medium'>
-                St. Louis, MO
+                {`${name}, ${country}`}
             </p>
 
         </div>
